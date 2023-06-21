caf1b67cbcaa11cd39fb7ee940b7f263e083a59d
Shahim <muhdshahim786@gmail.com>
2023-06-21 10:36:45 +0400

    added expressjs and run the project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const startServer = () => {};
+const express = require("express");
+const logger = require("./logger");
+const app = express();
+const port = process.env.PORT || 3000;
 
-module.exports = startServer();
+const startServer = () => {
+  app.listen(port, () => {
+    logger.info(`Server listening on port ${port}`);
+  });
+};
+
+module.exports = startServer;
